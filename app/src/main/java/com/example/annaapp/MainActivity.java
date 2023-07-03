b6120a9f0277b7def4775738c6ccf2e8f2e860d5
dinglequandale <konradatittel@gmail.com>
2023-07-03 16:58:23 -0500

    Updated System and Adding Settings OnClickListener

--- app/src/main/java/com/example/annaapp/MainActivity.java
+++ app/src/main/java/com/example/annaapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.annaapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -57,9 +60,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(R.id.action_settings == item.getItemId()){
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
