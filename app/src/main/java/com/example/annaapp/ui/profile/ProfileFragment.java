a9dd25630aabdceae17f63f7b74fce46fc940dbc
dinglequandale <konradatittel@gmail.com>
2023-07-15 06:22:30 -0500

    Improved layout designs, added edit show info functionality available only to the creator of the show

---
+++ app/src/main/java/com/example/annaapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,71 @@
+package com.example.annaapp.ui.profile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.annaapp.LoginActivity;
+import com.example.annaapp.MainActivity;
+import com.example.annaapp.R;
+import com.example.annaapp.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileFragment extends Fragment {
+    View view;
+    private FragmentProfileBinding binding;
+    private FirebaseAuth auth = FirebaseAuth.getInstance();;
+    private FirebaseUser user = auth.getCurrentUser();;
+
+    private Button btnLogout;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        if (user == null){
+            openLogin();
+        }
+        else {
+            //TODO:
+            // Set pfp of user
+            // Set login credentials
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                openLogin();
+            }
+        });
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textProfile;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+    public void openLogin(){
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+    }
+}
